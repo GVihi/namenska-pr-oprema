2499e492784d7507f87ffeef82426ee5460e0557
Gašper Viher <gasper.viher@gmail.com>
2021-03-26 17:27:07 +0100

    Started assignemt 2.2

---
+++ Vaja_2 Struktura podatkov/Unicode in UTF8/main.c
@@ -0,0 +1,7 @@
+#include "pretvori.c"
+
+int main(int argc, char** argv){
+    int in = preberiVhod(argv);
+    in = pretvoriZnak(in);
+    return 0;
+}
---
+++ Vaja_2 Struktura podatkov/Unicode in UTF8/pretvori.c
@@ -0,0 +1,16 @@
+#include "pretvori.h"
+#include <stdio.h>
+#include <stdlib.h>     
+
+int preberiVhod(char* cmd[]){
+    //strtol - convert string to a long integer
+    return (int) strtol(cmd[1], NULL, 16);
+}
+
+int pretvoriZnak(int znak){
+    
+}
+
+void izpisiZnak(int znak){
+
+}
